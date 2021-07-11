d2edda04a0429594e68c28b1b853bed2a53f8fa0
Kdoggg666 <kenanwright@gmail.com>
2021-07-11 13:01:37 +0000

    Fix issue with animal model attributes

--- animals/models.py
+++ animals/models.py
@@ -26,11 +26,13 @@ class Animal(models.Model):
     name = models.CharField(max_length=30)
     latin_name = models.CharField(max_length=30)
     climate = models.CharField(max_length=30)
+    temperament = models.CharField(max_length=50, default="")
+    food = models.CharField(max_length=50, default="")
     min_tank_size = models.CharField(max_length=30)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    # image_url = models.URLField(max_length=1024, null=True, blank=True)
-    # image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
