1215ae5f34dda0d6e46252f0f0bb336c2c87882e
Kdoggg666 <kenanwright@gmail.com>
2021-07-17 15:29:41 +0000

    Edit animal model, add 4 lizards to db

--- animals/models.py
+++ animals/models.py
@@ -34,7 +34,6 @@ class Animal(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
     temperature = models.CharField(max_length=30, default="")
     care_guide = models.TextField()
     cage_setup = models.TextField()
--- animals/test_views.py
+++ animals/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .models import Animal
-from .views import all_animals, animal_details
+from .views import all_animals, animal_details, all_animals_care, animal_care
 
 
 class TestViews(TestCase):
@@ -12,7 +12,17 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'animals/animals.html')
 
-    def test_animal_details(self):
+    def test_animal_details(self):  # Fix this 
         response = self.client.get('/animals/1/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'animals/animal_details.html')
+
+    def test_care_page(self): # Fix this
+        response = self.client.get('animals/care/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'animals/care.html')
+
+    def test_care_details(self): # Fix this
+        response = self.client.get('animals/care/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'animals/care_details.html')
