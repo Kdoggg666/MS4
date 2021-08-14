8ade0663749ca66753a98f0ab8324d859aacd869
Kdoggg666 <kenanwright@gmail.com>
2021-08-14 17:25:04 +0000

    Edit db url

--- GbgZoo/settings.py
+++ GbgZoo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['gbgzoo.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['https://gbgzoo.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -121,24 +121,22 @@ WSGI_APPLICATION = 'GbgZoo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if 'DATABASE_URL' not in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-    
+# if 'DATABASE_URL' not in os.environ:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
 
 
-
-# DATABASES = {
-#     'default': dj_database_url.parse('#')
-# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # DATABASES = {
 #     'default': {
